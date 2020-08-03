7f1519ef621f19f04423600c9dee00fb20d748a5
satorufukuhara <fukuhara@mse.mm.t.u-tokyo.ac.jp>
2020-08-03 16:10:39 +0900

    set up node

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-function hello(name: string): string {
-    return `Hello, ${name}!`
+import * as http from"http";
+
+class Main {
+    constructor() {
+      // set up HTTP server
+      const server: http.Server = http.createServer(
+          (request: http.IncomingMessage, response: http.ServerResponse) =>
+              this.requestHandler(request, response));
+      server.listen('5000');
+    }
+  
+
+    private requestHandler(request: http.IncomingMessage,
+                           response: http.ServerResponse): void {
+      response.end('Hello world');
+    }
 }
 
-console.log(hello("world"));
+const main = new Main();
